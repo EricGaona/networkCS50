275fe766877c2e3166bb663e6001a120eeeaa286
eric <ericgaona0413@gmail.com>
2025-01-10 13:16:42 +0000

    add the templates folder and the html files

---
+++ network/urls.py
@@ -0,0 +1,17 @@
+
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
+    path("all-posts", views.all_posts, name="all_posts"),
+    path("profile/<str:username>", views.profile, name="profile_page"),
+    path("following", views.following, name="following"),
+    path("like/<int:post_id>", views.like_unlike, name="like_unlike"),
+    path("follow/<str:username>", views.follow_unfollow, name="follow_unfollow"),
+    path("edit-post/<int:post_id>", views.edit_post, name="edit_post"),
+]
